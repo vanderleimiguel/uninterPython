8c868cb6363c4cf05100576a949a1755ca563602
vanderlei miguel <vanderleimiguel@hotmail.com>
2023-05-28 10:28:46 -0300

    questao 1

---
+++ Logica de Programao e Algoritimos/Trabalho/Questao_2.py
@@ -0,0 +1,10 @@
+# Questao 2 de 4
+# Aluno: Vanderlei do Nascimento Miguel Junior
+# RU: 4502606
+
+print("Bem vindo a Loja do Vanderlei do Nasc. Miguel Jr")
+print("")
+
+codigo = int(input("Entre com o codigo desejado: "))
+
+qtd = int(input("Entre com o valor da quantidade: "))
